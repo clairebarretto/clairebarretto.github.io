b64401a0d9bc2eb8e9c0bc4691a0dfa208d4608c
Claire Barretto <cbarretto@Claires-MacBook-Pro.local>
2016-02-24 00:12:36 -0800

    Changed resume layout, added landing section

--- js/main.js
+++ js/main.js
@@ -1,22 +1,4 @@
-$(function() {
-	// Fetch wordcloud file and initialize cloud plugin
-	$.get("files/wordcloud.txt").then(function(data) {
-		var skills = data.split("\n");
-		var words = skills.map(function(skill, i) {
-			var word = skill.split(",");
-			return {
-				text: word[0], 
-				weight: parseInt(word[1])
-			}
-		});
-		// Wordcloud initialization
-		$("#wordcloud").jQCloud(words, {	
-			autoResize: true
-		});
-	});
-});
-
-// Smooth scroll to div
+// Smoothly scroll to a section when a navigation link is clicked
 $(document).on("click", ".anchors a", function(evt) {
 	evt.preventDefault();
 
@@ -26,21 +8,127 @@ $(document).on("click", ".anchors a", function(evt) {
 	}, 500);
 });
 
-// Toggle between list/wordcloud for technical skills
-$(document).on("click", "#toggle-skills", function() {
-	var show = $(this).data("show");
-	if (show === "list") {
-		$("#wordcloud").fadeOut("slow", function() {
-			$("#skills-list").removeClass("hide").fadeIn("slow");
-		});
-		$(this).data("show", "cloud");
-		$(this).text("View as Cloud");
-	} 
-	else {
-		$("#skills-list").fadeOut("slow", function() {			
-			$("#wordcloud").removeClass("hide").fadeIn("slow");
-		});
-		$(this).data("show", "list");
-		$(this).text("View as List");
-	}
+// Make the navigation bar sticky after scrolling down past the canvas
+$(window).bind('scroll', function() {
+	var navHeight = $( window ).height() - 60;
+	 if ($(window).scrollTop() > navHeight) {
+		 $('header').addClass('fixed');
+	 }
+	 else {
+		 $('header').removeClass('fixed');
+	 }
 });
+
+// Particles.js configuration
+var config = {
+  "particles": {
+    "number": {
+      "value": 50,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "random" //00ffd1
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 4
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 14.617389821424212,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.49705773886831206,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+};
+
+particlesJS('canvas', config);
